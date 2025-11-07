a6154810d5fc30d3088ba277c616fa86cb3660e5
jose villegas <josevillegas13600@gmail.com>
2025-11-07 19:59:10 -0400

    arrays y listas

---
+++ src/main/kotlin/clase12/12_arrays_listas.kt
@@ -0,0 +1,24 @@
+package clase12
+
+fun main()
+{
+    //array
+    val emails = arrayOf("jose@empresa.com","carlos@empresa.com","wilson@empresa.com")
+    val cantidadDeCorreos = arrayOf(1,15,50,100)
+    println("El primer correo es: ${emails[0]}")
+    println("El segundo correo es: ${emails[1]}")
+    println("El tercer correo es: ${emails[2]}")
+    emails[0] ="nuevocorreo@empresa.com"
+    emails.set(0,"nuevocorreojose@empresa.com")
+    println("El primer correo es: ${emails[0]}")
+    println("La lista de correos es: ${emails.joinToString()}")
+    println("El tamaño del array es: ${emails.size}")
+    val nuevaListaDeEmails = mutableListOf<String>()
+    println("Nueva lista : ${nuevaListaDeEmails.joinToString()} tamaño: ${nuevaListaDeEmails.size}")
+    nuevaListaDeEmails.addAll(arrayOf("jose@empresa.com,carlos@empresa,wilson@empresa.com"))
+    println("Nueva lista : ${nuevaListaDeEmails.joinToString()} tamaño: ${nuevaListaDeEmails.size}")
+    nuevaListaDeEmails.remove("jose@empresa.com ")
+    println("Nueva lista : ${nuevaListaDeEmails.joinToString()} tamaño: ${nuevaListaDeEmails.size}")
+    nuevaListaDeEmails.add("jose@empresa.com ")
+    println("Nueva lista : ${nuevaListaDeEmails.joinToString()} tamaño: ${nuevaListaDeEmails.size}")
+}
