cd87e7b17b27bd05deaa187c2bc1bd7ac9d5966f
jose villegas <josevillegas13600@gmail.com>
2025-11-08 17:29:54 -0400

    clases

---
+++ src/main/kotlin/clase14/14_clases.kt
@@ -0,0 +1,52 @@
+package clase14
+
+class Email(
+    val asunto: String,
+    val remitente: String,
+    val mensaje: String
+)
+{
+    var leido: Boolean = false
+    fun marcarComoLeido(){
+        leido =true
+    }
+    fun marcarComoNoLeido()
+    {
+        leido = false
+    }
+    fun mostrarLaInfo()
+    {
+        println("De: $remitente | Asunto: $asunto | Leido: $leido")
+    }
+}
+class BandejaEmails
+{
+    val emails= mutableListOf<Email>()
+    fun agregarUnEmail(email:Email)
+    {
+        emails.add(email)
+    }
+    fun contarLeidos(): Int{
+        var contador = 0
+        for(email in emails)
+        {
+            if(!email.leido) {
+                contador++
+            }
+        }
+        return contador
+    }
+}
+fun main()
+{
+    val email1 = Email(asunto = "Reunion viernes", remitente = "carlos@gmail.com", mensaje = "No se te olvide nuestra reunion para el viernes")
+    val email2 = Email(asunto = "Reunion viernes", remitente = "carlos@gmail.com", mensaje = "No se te olvide nuestra reunion para el viernes")
+    val email3 = Email(asunto = "Reunion viernes", remitente = "carlos@gmail.com", mensaje = "No se te olvide nuestra reunion para el viernes")
+    email1.marcarComoLeido()
+    val bandejaEmails = BandejaEmails()
+    bandejaEmails.agregarUnEmail(email1)
+    bandejaEmails.agregarUnEmail(email2)
+    bandejaEmails.agregarUnEmail(email3)
+    println("Faltan por leer: ${bandejaEmails.contarLeidos()}")
+    println(email1.mostrarLaInfo())
+}
