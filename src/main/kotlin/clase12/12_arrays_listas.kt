a0a0f91283025f24e14a746dd022186d784b57a3
jose villegas <josevillegas13600@gmail.com>
2025-11-07 20:14:01 -0400

    listas mutables

--- src/main/kotlin/clase12/12_arrays_listas.kt
+++ src/main/kotlin/clase12/12_arrays_listas.kt
@@ -3,6 +3,7 @@ package clase12
 fun main()
 {
     //array
+
     val emails = arrayOf("jose@empresa.com","carlos@empresa.com","wilson@empresa.com")
     val cantidadDeCorreos = arrayOf(1,15,50,100)
     println("El primer correo es: ${emails[0]}")
@@ -15,10 +16,20 @@ fun main()
     println("El tamaño del array es: ${emails.size}")
     val nuevaListaDeEmails = mutableListOf<String>()
     println("Nueva lista : ${nuevaListaDeEmails.joinToString()} tamaño: ${nuevaListaDeEmails.size}")
-    nuevaListaDeEmails.addAll(arrayOf("jose@empresa.com,carlos@empresa,wilson@empresa.com"))
+    nuevaListaDeEmails.addAll(arrayOf("jose@empresa.com", "carlos@empresa", "wilson@empresa.com"))
     println("Nueva lista : ${nuevaListaDeEmails.joinToString()} tamaño: ${nuevaListaDeEmails.size}")
-    nuevaListaDeEmails.remove("jose@empresa.com ")
+    nuevaListaDeEmails.remove("jose@empresa.com")
     println("Nueva lista : ${nuevaListaDeEmails.joinToString()} tamaño: ${nuevaListaDeEmails.size}")
     nuevaListaDeEmails.add("jose@empresa.com ")
     println("Nueva lista : ${nuevaListaDeEmails.joinToString()} tamaño: ${nuevaListaDeEmails.size}")
+
+    val listOfEmails = listOf<String>("jose@empresa.com", "carlos@empresa.com", "wilson@empresa.com")
+    val nuevaLista = listOfEmails.subList(1,3)
+    println("Nueva lista: ${nuevaLista.joinToString()}")
+    val primerElemento = listOfEmails.first()
+    println("Primer Elemento: ${primerElemento}")
+    val ultimoElemento = listOfEmails.last()
+
+    val indexLastElement = listOfEmails.lastIndex
+    println("Ultimo Elemento: ${ultimoElemento} y su indice = $indexLastElement")
 }
